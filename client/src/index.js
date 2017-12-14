9c21bab54fa6f18dd8dc5ec99a5a541457e6e09c
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-14 13:56:50 -0500

    import store.js as store into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+import store from './store.js';
 import registerServiceWorker from './registerServiceWorker';
 
 
