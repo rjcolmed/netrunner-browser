01c65f3ba7f4960664e1aa7562c09e927144897e
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-14 16:25:56 -0500

    in SearchBar component, add constructor, with own-state-setting responsibility, and handleOnChange(), which fires thanks to onChange listener attached to text input. also add div that displays inputted text for testing purposes

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      query: ''
+    }
+  }
+
+  handleOnChange = (event) => {
+    const queryText = event.target.value
+    this.setState({
+      query: queryText
+    });
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Start typing..." />
-      </form>
+      <div>
+        <form>
+          <input 
+            onChange={this.handleOnChange} 
+            type="text" 
+            placeholder="Start typing..." 
+          />
+        </form>
+        <div>
+          {this.state.query}
+        </div>
+      </div>
+     
     );
   }
 }
