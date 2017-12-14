27029c340f38e45d0b0d084c4a7e9ffbeb2bd6ae
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-14 16:03:00 -0500

    create stubbed out CardShow component

---
+++ client/src/containers/CardShow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CardShow extends React.Component {
+  render() {
+    return (
+      <div>
+        Card Show Page
+      </div>
+    )
+  }
+}
+
+export default CardShow;
