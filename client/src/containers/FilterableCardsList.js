58d09b19ed46cb9c8db4c981a21041e0f183be30
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-14 15:05:35 -0500

    add compontDidMount with call to fire getAllCards() action, and mapDispatchToProps, which allows FilterableCardsList to access aforemention action

--- client/src/containers/FilterableCardsList.js
+++ client/src/containers/FilterableCardsList.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions/netrunnerdb_actions.js';
+import { bindActionCreators } from 'redux';
 import SearchBar from '../components/SearchBar';
 import CardsList from '../components/CardsList';
 
 class FilterableCardsList extends React.Component {
+
+  componentDidMount() {
+    if (this.props.cards.length === 0) {
+      this.props.actions.getAllCards();
+      console.log('fetched cards from netrunnerDb');
+    } else {
+      console.log('didn\'t need to fetch');
+    }
+  }
+
   render() {
     return (
       <div>
@@ -21,4 +33,10 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(FilterableCardsList);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilterableCardsList);
