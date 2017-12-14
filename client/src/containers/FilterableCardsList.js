91f2bf8c4d09ebb9319ada72287aebf3e6e552b5
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-14 14:27:13 -0500

    create FilterableCardsList

---
+++ client/src/containers/FilterableCardsList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class FilterableCardsList extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Filterable Cards List</h1>
+        {/* SearchBar */}
+        {/* CardsList */}
+      </div>
+    )
+  }
+}
+
+export default FilterableCardsList;
