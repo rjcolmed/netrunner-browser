8e3c7d62d128c42dae7baa0ae87821133035c71c
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-14 18:13:51 -0500

    change SearchBar's import statement to reflect its move into components/filters

--- client/src/containers/FilterableCardsList.js
+++ client/src/containers/FilterableCardsList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/netrunnerdb_actions.js';
 import { bindActionCreators } from 'redux';
-import SearchBar from '../components/SearchBar';
+import SearchBar from '../components/filters/SearchBar';
 import CardsList from '../components/CardsList';
 
 class FilterableCardsList extends React.Component {
