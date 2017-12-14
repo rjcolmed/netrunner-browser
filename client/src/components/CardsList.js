662c0540fa9a86da5b83dee5305bc14717b290ac
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-14 15:33:37 -0500

    import and user CardsListItem in CardsList

--- client/src/components/CardsList.js
+++ client/src/components/CardsList.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import CardsListItem from './CardsListItem';
 
 const CardsList = props =>
-  <div>This is the cards list...</div>
+  <div>
+    <h3>Inside the CardsList</h3>
+    <CardsListItem />
+  </div>
 
 export default CardsList;
