c6d8f644b2a215b767ec92594783cd329f346f98
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-14 17:51:15 -0500

    In CardsList component, render a CardsListItem for each card representation passed into CardsList

--- client/src/components/CardsList.js
+++ client/src/components/CardsList.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import CardsListItem from './CardsListItem';
 
-const CardsList = props =>
-  <div>
-    <h3>Inside the CardsList</h3>
-    <CardsListItem />
-  </div>
+const CardsList = props => {
+  function renderList() {
+    const cards = props.cards.map((card, index) => 
+      <CardsListItem card={ card } key={ index } />
+    ) 
+    return cards;
+  }
+
+  return (
+    <div>
+      <h3>Inside the CardsList</h3>
+      {renderList()}
+    </div>  
+  )
+}
+ 
 
 export default CardsList;
