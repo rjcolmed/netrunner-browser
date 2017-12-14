598480a7af1caac957eb8b5d36be53a57be89b78
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-14 14:28:00 -0500

    import FilterableCardsList into App.js, and several necessary components from react-router-dom

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FilterableCardsList from './FilterableCardsList';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={ logo } className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Netrunner Decks</h1>
         </header>
+      <FilterableCardsList />
       </div>
     );
   }
