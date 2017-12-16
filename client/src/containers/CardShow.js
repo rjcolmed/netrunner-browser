1d4a7e6f0914eb774e3440afc88ef990e8b778ff
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-16 12:05:47 -0500

    import bindActionCreators, connect. from redux and react-redux, respectively. add handleOnClick(), which fires addToFavorites action. add click listener to button, which fires handleOnClick upon click. change reference to state shape in mapStateToProps. add mapDispatchToProps

--- client/src/containers/CardShow.js
+++ client/src/containers/CardShow.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
- 
+import {  bindActionCreators } from 'redux';
+import * as actions from '../actions/favorites_actions'
+
 class CardShow extends React.Component {
+  handleOnClick = event => {
+    event.preventDefault();
+
+    this.props.actions.addToFavorites(this.props.card);
+
+  }
+
   render() {
     return (
       <div>
         <h1>{this.props.card.title}</h1>
+        <button onClick={ this.handleOnClick }>Add to Favorites</button>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const card = state.store.cards.find(card => card.code === ownProps.match.params.code);
+  const card = state.cards.find(card => card.code === ownProps.match.params.code);
 
   if(card) {
     return { card }
@@ -21,4 +31,12 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps)(CardShow);
+const mapDispatchToProps = (dispatch) => {
+  return  {
+    actions: bindActionCreators(actions, dispatch)
+  }  
+}
+ 
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardShow);
