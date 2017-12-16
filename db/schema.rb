5411c2df62eeb5db7c945c0efa8876ca0ef168f8
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-16 13:14:17 -0500

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216012426) do
+ActiveRecord::Schema.define(version: 20171216180900) do
 
   create_table "cards", force: :cascade do |t|
     t.string "code"
@@ -25,10 +25,18 @@ ActiveRecord::Schema.define(version: 20171216012426) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "favorites_lists", force: :cascade do |t|
-    t.integer "user_id"
+  create_table "favorites", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "list_id"
+    t.integer "card_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
   end
 
 end
