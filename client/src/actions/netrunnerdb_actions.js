dbd4c8912a459852293088c5c0da4491352f3b64
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-15 19:49:36 -0500

    add fetchAllFromNetrunnerDb() action, which seeds the 'local' db with card data from NetrunnerDb's API, and getAllCards(), which fetches cards from local API

--- client/src/actions/netrunnerdb_actions.js
+++ client/src/actions/netrunnerdb_actions.js
@@ -4,10 +4,28 @@ const NETRUNNER_DB_API = "https://netrunnerdb.com/api/2.0/public";
 
 export const getAllCards = () => {
   return dispatch => {
-    dispatch({ type: 'LOADING_ALL_CARDS' })
-    return fetch(`${NETRUNNER_DB_API}/cards`)
+    dispatch({ type: 'LOADING_CARDS' })
+    return fetch('/cards')
       .then(response => response.json())
-      .then(cards => dispatch({ type: 'GET_ALL_CARDS', payload: cards.data }))
-      .catch(error => console.log(error))
+      .then(cards => dispatch({ type: 'GET_ALL_CARDS', payload: cards }))
+      .catch(err => console.log(err))
+  }
+}
+
+export const fetchAllFromNetrunnerDb = () => {
+  return dispatch => {
+    dispatch({ type: 'FETCHING_ALL_CARDS' })
+    return fetch(`${NETRUNNER_DB_API}/cards`)
+              .then(response => response.json())
+              .then(cards => {
+                return fetch('/cards', {
+                  method: 'post',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                  },
+                  body: JSON.stringify({ cards: cards.data })
+                }).catch(err => console.log(err));
+              });
   }
 }
