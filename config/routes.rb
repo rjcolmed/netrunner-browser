d1371c77221ee1fbfbcaeeb433344507e56272ba
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-16 13:14:54 -0500

    change favorites_list symbol to favorites in order to reflect change of table in db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :cards, only: %i[index create]
 
-  resources :favorites_lists, only: %i[create show]
+  resources :favorites, only: %i[create show]
 end
