64b707a901b026a688af02ab8186869a675a237c
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-16 17:48:29 -0500

    change default user state to empty object and change return to reflect need to only keep one user in state per 'session'

--- client/src/reducers/users_reducer.js
+++ client/src/reducers/users_reducer.js
@@ -1,7 +1,7 @@
-export default function usersReducer(state = [], action) {
+export default function usersReducer(state = {}, action) {
   switch ( action.type ) {
     case 'ADD_USER':
-      return state.concat(action.user)
+      return action.user
     default:
       return state;
   }
