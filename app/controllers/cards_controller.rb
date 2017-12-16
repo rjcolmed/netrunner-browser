dcc435526b7ef31d9ec0bcf6cda84ab064cd0d3e
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-15 19:50:12 -0500

    add #index and #create to CardsController

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,20 @@
 class CardsController < ApplicationController
+  def index
+    render json: Card.all
+  end
+
   def create
-    
+    params[:cards].each do |card_params|
+      Card.create(
+        title: card_params[:title],
+        code: card_params[:code],
+        side_code: card_params[:side_code],
+        type_code: card_params[:type_code],
+        text: card_params[:text],
+        keywords: card_params[:keywords],
+        flavor: card_params[:flavor],
+        pack_code: card_params[:pack_code]
+      )
+    end
   end
 end
