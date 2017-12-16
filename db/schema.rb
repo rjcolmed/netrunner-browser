bb722992c00ec015c0ff0f9345742fc347f11de3
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-16 14:40:37 -0500

    create migrations to add password, password_confirmation, username, and email columns to users table, then migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216180900) do
+ActiveRecord::Schema.define(version: 20171216193854) do
 
   create_table "cards", force: :cascade do |t|
     t.string "code"
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20171216180900) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "password_digest"
+    t.string "username"
+    t.string "email"
+    t.string "password_confirmation"
   end
 
 end
