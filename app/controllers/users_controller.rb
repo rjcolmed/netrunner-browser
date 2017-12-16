692fad52d6e3673cd6d057bbb5f295ac9b2b011b
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-16 17:37:07 -0500

    add logic to #create to create new User using params from client-side form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
 
   def create
-   raise params.inspect
-
+    user = User.new(user_params)
+    
+    if user.save
+      render json: user
+    else
+      render json: { message: book.errors }, status: 400
+    end
   end
-
 
   private
 
