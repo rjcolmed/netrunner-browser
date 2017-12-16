da5c95ab3f864e64ac068d0ed42746ca1be943bc
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-16 13:16:30 -0500

    modify addToFavorites action to hit api in preparation for adding card to favorites list in db

--- client/src/actions/favorites_actions.js
+++ client/src/actions/favorites_actions.js
@@ -1,6 +1,19 @@
-export const addToFavorites = card => {
-  return {
-    type: 'ADD_TO_FAVORITES',
-    card
-  }
-}
+// export const addToFavorites = card => {
+//   return {
+//     type: 'ADD_TO_FAVORITES',
+//     card
+//   }
+// }
+
+// export const addToFavorites = card => {
+//   return dispatch => {
+//     dispatch({ type: 'ADDING_FAVORITE' });
+//     return fetch('/favorites', {
+//       method: 'post',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//       },
+//       body: JSON.stringify(card)
+//     });
+// }
