68202272ffc1bb4bf13fc06534ca59e18dbc019c
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-17 15:35:07 -0500

    rename 'favorite controller' to 'favorites_controller

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,21 @@
+class FavoritesController < ApplicationController
+  before_action :set_list, :set_card, :authenticate, only: %i[create index]
+
+  def index
+    
+  end
+
+  def create
+    favorite = Favorite.create(card: @card, list: @list)
+  end
+
+  private 
+
+  def set_card
+    @card = Card.find_by(code: params[:code])
+  end
+
+  def set_list
+    @list = List.find_or_create_by(user_id: current_user?.id)
+  end
+end
