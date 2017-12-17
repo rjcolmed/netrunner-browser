d96eacb077c7908fadf7d894dcea36ace90f0677
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-17 11:19:21 -0500

    add all of client's existing action types to action_types, export them.

--- client/src/actions/action_types.js
+++ client/src/actions/action_types.js
@@ -1,1 +1,13 @@
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+
+export const LOADING_CARDS = 'LOADING_CARDS';
+
+export const GET_ALL_CARDS = 'GET_ALL_CARDS';
+
+export const FETCHING_ALL_CARDS = 'FETCHING_ALL_CARDS';
+
+export const ADD_USER = 'ADD_USER';
+
+export const CREATING_USER = 'CREATING_USER';
+
+export const LOGGING_USER_IN = 'LOGGING_USER_IN';
