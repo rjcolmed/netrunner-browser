b82f47f8b91cdf585bbadac2da53277a520722ce
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-17 15:31:57 -0500

    create favorites_api.js and add FavoritesApi JS class declaration with static methods favorite() and fetchFavorites()

---
+++ client/src/api/favorites_api.js
@@ -0,0 +1,41 @@
+import fetch from 'isomorphic-fetch';
+
+class FavoritesApi {
+
+  static requestHeaders() {
+    return { 
+      'AUTHORIZATION': `Bearer ${sessionStorage.jwt}`
+     }
+  }
+
+
+  static favorite(card) {
+    const request = new Request('/favorites', {
+      method: 'post',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'AUTHORIZATION': `Bearer ${sessionStorage.jwt}`
+      }),
+      body: JSON.stringify(card)
+    });
+
+    return fetch(request)
+      .then(response => response.json())
+      .catch(err => console.log(err))
+  }
+
+  static fetchFavorites() {
+    const headers = this.requestHeaders();
+
+    const request = new Request('/favorites', {
+      method: 'get',
+      headers: headers
+    });
+
+    return fetch(request)
+      .then(response => response.json())
+      .catch(err => console.log(err));
+  }
+}
+
+export default FavoritesApi;
