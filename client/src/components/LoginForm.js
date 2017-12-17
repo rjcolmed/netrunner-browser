c519ff55ec8695f6896d2d186381f51a2d52ad4c
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-16 19:09:04 -0500

    build out LoginForm component

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,15 +1,60 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { loginUser } from '../actions/users_actions'
 import TextInput from './TextInput';
 
-class LoginForm extends React.Component {
+class SignupForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [event.target.name]: event.target.value
+      }
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.loginUser(this.state.user);
+  }
+
   render() {
     return (
-      <form>
-        <TextInput placeholder='username' />
-        <TextInput placeholder='password' />
+      <form onSubmit={ this.handleOnSubmit }>
+        <TextInput 
+          name='username' 
+          placeholder='username' 
+          handleOnChange={this.handleOnChange}
+          value={ this.state.user.username }
+        />
+        <TextInput 
+          name='password' 
+          placeholder='password' 
+          handleOnChange={this.handleOnChange}
+          value={ this.state.user.password }
+        />
+        <button type='submit'>Submit</button>
       </form>
     )
   }
 }
 
-export default LoginForm;
+function mapDispatchToProps(dispatch) {
+  return {
+    loginUser: bindActionCreators(loginUser, dispatch)
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SignupForm);
