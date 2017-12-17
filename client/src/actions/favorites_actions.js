17a787cd7357a80bba6b67f596b1348048f5c4a1
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-17 15:33:32 -0500

    remove fetch calls from favorites_actions and place them in favorites_api

--- client/src/actions/favorites_actions.js
+++ client/src/actions/favorites_actions.js
@@ -1,19 +1,19 @@
-// export const addToFavorites = card => {
-//   return {
-//     type: 'ADD_TO_FAVORITES',
-//     card
-//   }
-// }
+// import * as types from './action_types';
+import FavoritesApi from '../api/favorites_api';
 
-// export const addToFavorites = card => {
-//   return dispatch => {
-//     dispatch({ type: 'ADDING_FAVORITE' });
-//     return fetch('/favorites', {
-//       method: 'post',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json'
-//       },
-//       body: JSON.stringify(card)
-//     });
-// }
+export const addToFavorites = card => {
+  return dispatch => {
+    // dispatch({ type: types.ADD_TO_FAVORITES });
+    return FavoritesApi.favorite(card)
+      .then(response => response.json())
+      .catch(err => console.log(err));
+  }
+}
+
+export const getFavorites = () => {
+  return dispatch => {
+    return FavoritesApi.fetchFavorites()
+      .then(response => response.json())
+      .then()
+  }
+}
