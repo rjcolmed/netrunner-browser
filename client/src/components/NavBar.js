7e7d216689d7adbcb42951dabed1c5d64360209a
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-18 00:10:03 -0500

    make NavBar a container, add handleOnClick and associated click listener to new Log Out NavLink. add conditional to NavBar's render() to check if user is logged in

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,41 +1,91 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions/session_actions';
 
-const NavBar = props => {
-  return (
-    <ul className="navBar">
-      <li className="navBarLink">
-        <NavLink
-          exact to="/"
-          >Home
-        </NavLink>
-      </li>
-      <li className="navBarLink">
-        <NavLink
-          exact to="/login"
-          >Login
-        </NavLink>
-      </li>
-      <li className="navBarLink">
-        <NavLink
-          exact to="/signup"
-          >Sign Up
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          exact to="/cards"
-          >Cards
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          exact to="/cards/favorites"
-          >Favorites
-        </NavLink>
-      </li>
-     </ul>
-    );
+class NavBar extends React.Component {
+
+  handleOnClick = (event) => {
+    event.preventDefault();
+
+    this.props.actions.logOutUser();
   }
 
-export default NavBar;
+  render() {
+    if (this.props.logged_in) {
+      return (
+        <ul className="navBar">
+          <li className="navBarLink">
+            <NavLink
+              exact to="/"
+              >Home
+            </NavLink>
+          </li>
+          <li className="navBarLink">
+            <NavLink
+              exact to="/logout"
+              onClick={ this.handleOnClick }
+              >Logout
+            </NavLink>
+          </li>
+          <li className="navBarLink">
+            <NavLink
+              exact to="/signup"
+              >Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact to="/cards"
+              >Cards
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact to="/cards/favorites"
+              >Favorites
+            </NavLink>
+          </li>
+         </ul>
+        );
+    } else {
+      return (
+        <ul className="navBar">
+          <li className="navBarLink">
+            <NavLink
+              exact to="/"
+              >Home
+            </NavLink>
+          </li>
+          <li className="navBarLink">
+            <NavLink
+              exact to="/login"
+              >Login
+            </NavLink>
+          </li>
+          <li className="navBarLink">
+            <NavLink
+              exact to="/signup"
+              >Sign Up
+            </NavLink>
+          </li>
+         </ul>
+        );
+    }
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    logged_in: state.session
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
