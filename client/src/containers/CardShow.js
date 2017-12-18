022316c71e28ed86aa8d8e1795f4520bb4c1f183
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-18 01:31:15 -0500

    import react-router, add constructor and component-level state to CardShow component with 'redirect' key, add removeFromFavorites(), and conditional to render() to redirect upon removal of favorite

--- client/src/containers/CardShow.js
+++ client/src/containers/CardShow.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {  bindActionCreators } from 'redux';
-import * as favoritesActions from '../actions/favorites_actions'
+import { Redirect } from 'react-router';
+import * as actions from '../actions/favorites_actions'
 
 class CardShow extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      redirect: false
+    }
+  }
 
   handleOnClick = event => {
     event.preventDefault();
-    this.props.favoritesActions.addToFavorites(this.props.card);
+    this.props.actions.addToFavorites(this.props.card);
+  }
+
+  removeFromFavorites = event => {
+    event.preventDefault();
+
+    this.props.actions.removeFromFavorites(this.props.card)
+      .then(() => this.setState({ redirect: true }));
   }
 
   render() {
-    return (
-      <div>
-        <h1>{this.props.card.title}</h1>
-        <button onClick={ this.handleOnClick }>Add to Favorites</button>
-      </div>
-    )
+    const { redirect } = this.state;
+    const { favorites, card } = this.props
+
+    if ( redirect ) {
+      return <Redirect to='/cards/favorites' />
+    }
+
+    if (favorites.find(favorite => favorite.id === this.props.card.id)) {
+      return (
+        <div>
+          <h1>{ card.title }</h1>
+            <button onClick={ this.removeFromFavorites }>Remove from Favorites</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>{ card.title }</h1>
+          <button onClick={ this.handleOnClick }>Add to Favorites</button>
+        </div>
+      );
+    }
   }
 }
 
@@ -24,18 +55,23 @@ const mapStateToProps = (state, ownProps) => {
   const card = state.cards.find(card => card.code === ownProps.match.params.code);
 
   if(card) {
-    return { card }
+    return { 
+      card:card,
+      favorites: state.favorites 
+    }
   } else {
-    return { card: {} }
+    return { 
+      card: {} ,
+      favorites: state.favorites
+    }
   }
+
 }
 
 const mapDispatchToProps = (dispatch) => {
   return  {
-    favoritesActions: bindActionCreators(favoritesActions, dispatch)
+    actions: bindActionCreators(actions, dispatch)
   }  
 }
- 
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(CardShow);
