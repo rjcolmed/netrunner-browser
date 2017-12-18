6dff6cfb3dfb9ce7c2b6a4e4340ce82e09079189
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-18 12:59:06 -0500

    add several semanti ui components to LandingPage.js

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Container, Header, Button, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () =>
-  <div>
-    <h1>Welcome to NetrunnerDecks Browser</h1>
-  </div>
-
+  <Container>
+    <Container className="landing-page">
+    <Header size="huge">Welcome to NetrunnerDecks Browser</Header>
+    <Button.Group attached="bottom">
+      <Button as={ Link } to="/signup">Sign Up</Button>
+      <Button as={ Link } to="/login">Log In</Button>
+    </Button.Group>
+  </Container>
+  </Container>
 export default LandingPage;
