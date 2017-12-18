ce3631a4a0f4b46ae72d63ca6133a4eb0d01b85f
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-18 02:00:09 -0500

    add CREATE_USER_SUCCESS switch case to sessionReducer

--- client/src/reducers/session_reducer.js
+++ client/src/reducers/session_reducer.js
@@ -3,9 +3,10 @@ import * as types from '../actions/action_types';
 export default function sessionReducer(state = !!sessionStorage.jwt, action) {
   switch ( action.type ) {
     case types.LOG_IN_SUCCESS:
-    //need to redirect to allowed page after authentication. how?
       return !!sessionStorage.jwt
     case types.LOG_OUT:
+      return !!sessionStorage.jwt
+    case types.CREATE_USER_SUCCESS:
       return !!sessionStorage.jwt
     default:
       return state;
