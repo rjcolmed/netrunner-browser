567a329a435f0cbedd5f6ebeb2dfbacf77ab5f7d
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-17 23:03:45 -0500

    import FavoritesList component into CardsPage and replace FilterableCards component prop of favorites route with it

--- client/src/components/CardsPage.js
+++ client/src/components/CardsPage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import FilterableCardsList from '../containers/FilterableCardsList';
 import CardShow from '../containers/CardShow';
+import FavoritesList from '../containers/FavoritesList';
 
 const CardsPage = () =>
   <Switch>
     <Route exact path='/cards' component={ FilterableCardsList } />
-    <Route exact path='/cards/favorites' component={ FilterableCardsList } />
+    <Route exact path='/cards/favorites' component={ FavoritesList } />
     <Route exact path='/cards/:code' component={ CardShow } />
   </Switch>
 
