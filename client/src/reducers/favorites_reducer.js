0263fe236dae78563f433cc88880486824062daa
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-17 23:01:52 -0500

    remove call to concat on state and replace with returning whole favorites list fetched from db

--- client/src/reducers/favorites_reducer.js
+++ client/src/reducers/favorites_reducer.js
@@ -3,7 +3,7 @@ import * as types from '../actions/action_types';
 export default function favoritesReducer(state = [], action) {
   switch ( action.type ) {
     case types.SET_FAVORITES:
-      return state.concat(action.favorites)
+      return action.favorites
     default:
       return state;
   }
