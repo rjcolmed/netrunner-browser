64cd5cb7a521a94a3d3bfe4098de441a3fa97b5f
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-17 22:25:21 -0500

    rearrange favorites actions, add setFavorites()

--- client/src/actions/favorites_actions.js
+++ client/src/actions/favorites_actions.js
@@ -1,19 +1,27 @@
-// import * as types from './action_types';
+import * as types from './action_types';
 import FavoritesApi from '../api/favorites_api';
 
 export const addToFavorites = card => {
   return dispatch => {
-    // dispatch({ type: types.ADD_TO_FAVORITES });
     return FavoritesApi.favorite(card)
-      .then(response => response.json())
+      .then(response => response)
       .catch(err => console.log(err));
+  }
+}
+
+const setFavorites = favorites => {
+  return {
+    type: types.SET_FAVORITES,
+    favorites
   }
 }
 
 export const getFavorites = () => {
   return dispatch => {
     return FavoritesApi.fetchFavorites()
-      .then(response => response.json())
-      .then()
+      .then(favorites =>  {
+        dispatch(setFavorites(favorites));
+      })
+      .catch(err => console.log(err));
   }
 }
