b72b2337681d104d3cb37a8f72e07f631e416fba
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-17 23:02:59 -0500

    create FavoritesList.js and add working FavoritesList component therein

---
+++ client/src/containers/FavoritesList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import CardsList from '../components/CardsList';
+import * as favoritesActions from '../actions/favorites_actions'
+
+class FavoritesList extends React.Component {
+
+  componentDidMount() {
+    this.props.favoritesActions.getFavorites();
+  }
+
+  render() {
+    return (
+      <div>
+        <CardsList 
+          cards={ this.props.favorites }
+        />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    favorites: state.favorites
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    favoritesActions: bindActionCreators(favoritesActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FavoritesList);
