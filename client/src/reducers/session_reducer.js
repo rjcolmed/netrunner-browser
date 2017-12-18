fd44e148e0b8d08db698d7404fbdeea6aac3d52f
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-18 00:08:45 -0500

    add LOG_OUT case to sessions_reducer

--- client/src/reducers/session_reducer.js
+++ client/src/reducers/session_reducer.js
@@ -5,6 +5,8 @@ export default function sessionReducer(state = !!sessionStorage.jwt, action) {
     case types.LOG_IN_SUCCESS:
     //need to redirect to allowed page after authentication. how?
       return !!sessionStorage.jwt
+    case types.LOG_OUT:
+      return !!sessionStorage.jwt
     default:
       return state;
   }
