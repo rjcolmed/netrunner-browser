cb1272c0fc86b9ff2105e3cb509b5286c9620c24
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-17 23:15:41 -0500

    change default state of SessionReducer from false to !!sessionStorage.jwt

--- client/src/reducers/session_reducer.js
+++ client/src/reducers/session_reducer.js
@@ -1,6 +1,6 @@
 import * as types from '../actions/action_types';
 
-export default function sessionReducer(state = false, action) {
+export default function sessionReducer(state = !!sessionStorage.jwt, action) {
   switch ( action.type ) {
     case types.LOG_IN_SUCCESS:
     //need to redirect to allowed page after authentication. how?
