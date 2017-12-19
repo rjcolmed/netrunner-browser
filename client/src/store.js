c794826d7079b7808ff34ba9b23e04b521becda4
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-18 20:24:00 -0500

    remove usersReducer from store.js

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import cardsReducer from './reducers/cards_reducer';
 import favoritesReducer from './reducers/favorites_reducer';
-// import usersReducer from './reducers/users_reducer';
 import sessionReducer from './reducers/session_reducer';
 
 const rootReducer = combineReducers({
