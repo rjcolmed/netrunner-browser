e1510c91921eda8a882641b079f35ef996ec8f8d
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-18 20:24:31 -0500

    add Rote path to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import store from './store.js';
 import registerServiceWorker from './registerServiceWorker';
+import createBrowserHistory from 'history/createBrowserHistory';
 
+const history = createBrowserHistory();
 
 
 ReactDOM.render(
   <Provider store={ store }>
-    <Router>
-      <App/>
+    <Router history={ history }>
+      <Route path="/:filter?" component={ App }/>
+      {/* <App/> */}
     </Router>
   </Provider>
 , document.getElementById('root'));
