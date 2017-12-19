d196c810b07e1fa63fedc6837ac43d947ad6833c
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-19 12:53:03 -0500

    import types from action_types.js into cards_reducer.js and replace action strings with types

--- client/src/reducers/cards_reducer.js
+++ client/src/reducers/cards_reducer.js
@@ -1,9 +1,13 @@
+import * as types from '../actions/action_types';
+
 export default function cardsReducer(state = [], action) {
   switch ( action.type ) {
     case 'LOADING_ALL_CARDS':
       return Object.assign({}, state, { loading: true });
-    case 'GET_ALL_CARDS':
+    case types.GET_ALL_CARDS:
       return action.cards
+    case types.ADDING_CARDS_TO_API:
+      return state;
     default:
       return state;
   }
