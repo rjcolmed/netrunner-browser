dbba577e3cb2d76d63a7d2c65011c8ac1d2b3179
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-19 10:54:38 -0500

    add REMOVING_FROM_FAVORITES action case to favoritesReducer

--- client/src/reducers/favorites_reducer.js
+++ client/src/reducers/favorites_reducer.js
@@ -4,6 +4,8 @@ export default function favoritesReducer(state = [], action) {
   switch ( action.type ) {
     case types.SET_FAVORITES:
       return action.favorites
+    case types.REMOVING_FROM_FAVORITES:
+      return state;
     default:
       return state;
   }
