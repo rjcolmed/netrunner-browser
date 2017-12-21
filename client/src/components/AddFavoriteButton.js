a89e661740b91c6a4856ebe36b0f6b2725eda9d8
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-21 18:57:57 -0500

    create AddFavoriteButton.js and add AddFavorite button component therein

---
+++ client/src/components/AddFavoriteButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+const AddFavoriteButton = ({ addToFavorites }) => (
+  <Button onClick={ addToFavorites } fluid>
+    Add To Favorites
+  </Button>
+)
+
+
+export default AddFavoriteButton;
