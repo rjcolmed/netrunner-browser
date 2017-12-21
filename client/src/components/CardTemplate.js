bfc2040c827dd4bd141f4cad73521ebcd287aa8c
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-21 18:58:31 -0500

    create CardTemplate.js and add CardTemplate component therein

---
+++ client/src/components/CardTemplate.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image } from 'semantic-ui-react';
+
+class CardTemplate extends React.Component {
+  static propTypes = {
+    button: PropTypes.node,
+    card: PropTypes.node
+  }
+
+  render() {
+    const { card, button } = this.props;
+
+    return (
+      <div>
+        <Card>
+          <Image src={ card.image_url } />
+          <Card.Content>
+            <Card.Header>{ card.title }</Card.Header>
+            <Card.Meta>{ card.flavor }</Card.Meta>
+            <Card.Description>{ card.text }</Card.Description>
+          </Card.Content>
+
+          <span>{ button }</span>
+        </Card>
+      </div>
+    )
+  }
+}
+
+export default CardTemplate;
