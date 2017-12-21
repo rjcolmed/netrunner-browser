ce65e145ac2b6ceb5b86e6a5a06008a5ecb8bda3
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-21 10:24:45 -0500

    replace string action case in cards_reducer.js with corresponding  action type from action_types.js

--- client/src/reducers/cards_reducer.js
+++ client/src/reducers/cards_reducer.js
@@ -2,7 +2,7 @@ import * as types from '../actions/action_types';
 
 export default function cardsReducer(state = [], action) {
   switch ( action.type ) {
-    case 'LOADING_ALL_CARDS':
+    case types.LOADING_CARDS:
       return Object.assign({}, state, { loading: true });
     case types.GET_ALL_CARDS:
       return action.cards
