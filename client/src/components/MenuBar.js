7d6428a80e79123063df00026a2b2f297108a5cb
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-22 11:39:55 -0500

    create MenuBar.js and MenuBar component therein

---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions/session_actions';
+import * as cardActions from '../actions/cards_actions.js';
+import Menu from './Menu';
+
+class MenuBar extends React.Component {
+  state = {
+    activeItem: 'home'
+  }
+
+  handleItemClick = (event, { name }) => this.setState({
+    activeItem: name
+  })
+
+  logUserOut = event => {
+    this.props.actions.logOutUser();
+    this.props.history.push('/');
+  }
+
+  fetchCardsFromNetrunnerDb =  (event) => {
+    event.preventDefault();
+
+    this.props.cardActions.fetchAllFromNetrunnerDb();
+  }
+
+  render() {
+
+    const { activeItem } = this.state;
+    const { loggedIn } = this.props;
+
+    return (
+      <Menu
+        loggedIn={ loggedIn }
+        activeItem={ activeItem }
+        logUserOut={ this.logUserOut }
+        handleItemClick={ this.handleItemClick }
+        fetchCardsFromNetrunnerDb={ this.fetchCardsFromNetrunnerDb }
+      />
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.session
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(actions, dispatch),
+    cardActions: bindActionCreators(cardActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MenuBar);
