8ca21c8ae5bf5947a06f5bba6ca8bc3797213672
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-22 11:40:31 -0500

    create MenuTemplate.js and its corresponding component

---
+++ client/src/components/MenuTemplate.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+class MenuTemplate extends React.Component {
+
+  render() {
+    const { activeItem, handleItemClick, authActions } = this.props;
+
+    return (
+      <Menu stackable pointing secondary>
+        <Menu.Item 
+          as= { Link } 
+          to="/" name="home" 
+          active={ activeItem === "home" } 
+          onClick={ handleItemClick }>
+          Home
+        </Menu.Item>
+        <Menu.Item 
+          as={ Link } to="/cards" 
+          name="cards" 
+          active={ activeItem === "cards" } 
+          onClick={ handleItemClick }>
+          Cards
+        </Menu.Item>
+        <Menu.Menu position="right">
+          { authActions }
+        </Menu.Menu>
+      </Menu>
+    )
+  }
+}
+
+export default MenuTemplate;
