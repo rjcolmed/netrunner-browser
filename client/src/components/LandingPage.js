8034d66935f5f586eb39e004a347b14cff9670a2
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-24 11:42:43 -0500

    reposition header in LandingPage.js and add inverted property thereto from semantic ui

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 
 const LandingPage = () =>
   <Container>
+    <Header size="huge" textAlign="center" inverted>Welcome to NetrunnerDecks Browser</Header>
     <Container className="landing-page">
-    <Header size="huge">Welcome to NetrunnerDecks Browser</Header>
-    <Button.Group attached="bottom">
-      <Button as={ Link } to="/signup">Sign Up</Button>
-      <Button as={ Link } to="/login">Log In</Button>
-    </Button.Group>
-  </Container>
+      <Button.Group attached="bottom">
+        <Button as={ Link } to="/signup">Sign Up</Button>
+        <Button as={ Link } to="/login">Log In</Button>
+      </Button.Group>
+    </Container>
   </Container>
 export default LandingPage;
